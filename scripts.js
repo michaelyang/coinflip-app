7274c6578cf2d85aa4f0f7045668147964937853
Michael Yang <myang@Michael-Yang.cable.rcn.com>
2018-12-15 13:43:00 -0500

    changing to highcharts

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ inputFields.forEach(inputField =>
   })
 );
 submitButton.addEventListener("click", handleSubmit);
+/*
 let graphData = [];
 let context = document.getElementById("graph").getContext("2d");
 let data = {
@@ -34,6 +35,7 @@ let graph = new Chart(context, {
   data: data,
   options: options
 });
+*/
 
 function handleSubmit() {
   const [
@@ -46,11 +48,12 @@ function handleSubmit() {
   let data = [].concat.apply(
     [],
     Array.from({ length: numberOfTrials }, () =>
-      createData(winRate, winAmount, loseAmount, betPerTrial)
+      createHCData(winRate, winAmount, loseAmount, betPerTrial)
     )
   );
-  updateData(data);
-  updateGraph(graph);
+  updateHC(data, betPerTrial);
+  //updateData(data);
+  //updateGraph(graph);
 }
 
 function createData(winRate, winAmount, loseAmount, betPerTrial) {
@@ -67,7 +70,76 @@ function createData(winRate, winAmount, loseAmount, betPerTrial) {
     ],
     []
   );
+  console.log(data);
   return cumulativeData;
+}
+
+function createHCData(winRate, winAmount, loseAmount, betPerTrial) {
+  let generatedData = Array.from({ length: betPerTrial }, () => Math.random());
+  let cumulativeData = generatedData.reduce(
+    (prev, curr, i) => [
+      ...prev,
+      [
+        i + 1,
+        (curr < winRate ? winAmount : -1 * loseAmount) +
+          (prev[i - 1] ? prev[i - 1][1] : 0)
+      ]
+    ],
+    []
+  );
+  return cumulativeData;
+}
+
+function updateHC(data, maxX) {
+  if (!Highcharts.Series.prototype.renderCanvas) {
+    throw "Module not loaded";
+  }
+
+  Highcharts.chart("graph", {
+    chart: {
+      zoomType: "xy",
+      height: "100%"
+    },
+    boost: {
+      useGPUTranslations: true,
+      usePreAllocated: true
+    },
+    xAxis: {
+      min: 0,
+      max: maxX,
+      gridLineWidth: 1,
+      title: {
+        text: "Number of Bets"
+      }
+    },
+    yAxis: {
+      minPadding: 0,
+      maxPadding: 0.05,
+      title: {
+        text: "Payout ($)"
+      }
+    },
+    title: {
+      text: null
+    },
+    legend: {
+      enabled: false
+    },
+    series: [
+      {
+        type: "scatter",
+        color: "rgba(152,0,67,0.2)",
+        data: data,
+        marker: {
+          radius: 1
+        },
+        tooltip: {
+          followPointer: false,
+          pointFormat: "[{point.x:.1f}, {point.y:.1f}]"
+        }
+      }
+    ]
+  });
 }
 
 function updateData(data) {
@@ -77,3 +149,4 @@ function updateData(data) {
 function updateGraph(graph) {
   graph.update();
 }
+handleSubmit();
